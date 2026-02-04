08ff5b9d6cfd7ba863c9c0710b250c81786f9427
Santi <learnation.ops@gmail.com>
2026-02-04 19:02:41 -0300

    Debug: Add frontend version log to verify deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
 
+
+console.log('Frontend Version: 2024-02-04 JWT Patch v2');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeProvider>
