7821496a372d626622183103c72b676ace547c97
Santi <learnation.ops@gmail.com>
2026-04-13 09:56:27 -0300

    Refix

--- frontend/src/components/modals/GlobalSettingsModal.jsx
+++ frontend/src/components/modals/GlobalSettingsModal.jsx
@@ -4,6 +4,7 @@ import { X, Settings } from 'lucide-react';
 import { useAuth } from '../../contexts/AuthContext';
 import SettingsPage from '../../pages/admin/settings/SettingsPage';
 import CloserSettingsPage from '../../pages/closer/settings/SettingsPage';
+//Minor Change
 
 const GlobalSettingsModal = ({ isOpen, onClose }) => {
     const { user } = useAuth();
